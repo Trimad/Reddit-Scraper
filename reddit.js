b21ebce8f54963262dd9a442269e7af196521ced
Trimad <mattedinnards@gmail.com>
2021-03-13 11:57:49 -0500

    first commit

---
+++ reddit.js
@@ -0,0 +1,85 @@
+const snoowrap = require('snoowrap');
+const express = require('express')
+const hostname = '127.0.0.1';
+const port = 3000;
+const app = express()
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+const r = new snoowrap({
+  userAgent: 'test',
+  clientId: 'h0MDf1fkbsNcIA',
+  clientSecret: '7fh2ad7l49IgWSzCXSY4Vfag4keySA',
+  refreshToken: '845574849900-cJydmA2JRSrpjRYsKzBgenO-huVzhg'
+});
+
+var posts_groomed = [];
+
+/*********************************
+* Description: 
+* Usage example: localhost:3000/subreddit/top?name=TwoSentenceHorror&time=all&llimit=2
+**********************************/
+app.get('/subreddit/top', async (request, response) => {
+  let n = request.query.name;
+  let t = request.query.time;
+
+  const subreddit = await r.getSubreddit(n);
+  const posts = await subreddit.getTop({ time: t });
+
+  posts_groomed.splice(0, posts_groomed.length);//empty the array
+
+  posts.forEach(post => {
+    posts_groomed.push({
+      link: post.url,
+      title: post.title,
+      id: post.id
+    })
+  });
+
+  response.send(posts_groomed);
+
+});
+
+/*********************************
+* Description: Gets an object containing the title, body, and an array of comments from a post ID.
+* Usage example: localhost:3000/submission?id=m2pd90
+**********************************/
+app.get('/submission', async (request, response) => {
+  let id = request.query.id;
+  let submission = await r.getSubmission(id).fetch();
+  let obj = { "title": submission.title, "selftext": submission.selftext, "comments": recurseComments(submission.comments, []) };
+  response.send(obj);
+  function recurseComments(comments, arr) {
+    try {
+      for (comment of comments) {
+        arr.push(comment.body);
+        if (comment.replies.length > 0) { recurseComments(comment.replies, arr); }
+      }
+    }
+    catch (err) {
+      console.log(err);
+    }
+    return arr;
+  }
+});
+
+
+
+
+
+// function getSubmissionComments(id) {
+//   let submission = r.getSubmission(id).fetch().expandReplies({ limit: 100, depth: 10 });
+//   //r.getSubmission(asdf).expandReplies().then(thread => { thread.comments.forEach((comment) => whatever); });
+//   return submission;
+// }
+
+
+
+//Get Subreddit Rules
+app.get('/getsubredditrules', (req, res) => {
+  r.getSubreddit(subreddit).getRules().then(function (response) {
+    res.send(response);
+  });
+});
